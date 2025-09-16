b4d1c569aead501c6691f5dcc7ef9379d55e96cc
PeterLukyanov <petr.lukyanov.96@gmail.com>
2025-09-16 10:25:56 +0300

    Add gitignore for PuppeteerSharp

--- TemplatesManager/obj/Debug/net9.0/TemplatesManager.AssemblyInfo.cs
+++ TemplatesManager/obj/Debug/net9.0/TemplatesManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemplatesManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cebb375911e2a1068eacd2dbf99e06c6bf2c979")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0672efcb1fe4359cb3a156e0c46ebeb47408d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemplatesManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemplatesManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
